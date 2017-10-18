11212c4e0c58618290680b82adcbf876f9227f3c
braulioagr3 <braulioagr3@gmail.com>
2017-10-18 11:01:37 -0500

    Ejemplo Excepciones

--- src/EjemploExcepciones.java
+++ src/EjemploExcepciones.java
@@ -1,10 +1,32 @@
-/**
- *
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 public class EjemploExcepciones
 {
-    public static void main(String[] arg)
+    public static void main(String[] arg) //throws FileNotFoundException
     {
-        System.out.println("Hola");
+        try
+        {
+            File archivo = new File("entrada.txt");
+            Scanner entrada = new Scanner(archivo);
+            System.out.print("Dame el primer número: ");
+            int num1 = entrada.nextInt();
+            System.out.print("Dame el segundo número: ");
+            int num2 = entrada.nextInt();
+            System.out.println("La division es:" + num1/num2);
+        }
+        catch (ArithmeticException eA)
+        {
+            System.out.println("Division por cero");
+        }
+        catch (InputMismatchException eE)
+        {
+            System.out.println("Entrada Invalida");
+        }
+        catch (FileNotFoundException eF)
+        {
+            System.out.println("No existe el Archivo");
+        }
     }
 }
